39c5c6901e7796b6a877376c43841d0c1585763a
DannBS89 <danilo.bsilva89@yahoo.com.br>
2024-11-06 13:17:11 -0300

    destroier

--- web/docs/backup/treco/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("meuForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    document.getElementById("resultado").textContent = `Nome: ${nome}, Email: ${email}`;
-});
-
